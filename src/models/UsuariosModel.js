ca46278412283212747d13d74725b5942e150aff
YohanBZ <barbozayohan@gmail.com>
2023-09-15 15:00:06 -0300

    feat: :sparkles: Criação do arquivo UsuariosModel que contém a model de Usuarios

---
+++ src/models/UsuariosModel.js
@@ -0,0 +1,18 @@
+/**
+ * Objeto modelo para usuários
+ */
+class UsuariosModel{
+  /**
+   * Construtor do objeto modelo para usuários
+   * @param {string} nome 
+   * @param {string} email 
+   * @param {string} telefone 
+   */
+  constructor(nome, email, telefone){
+      this.nome = nome
+      this.email = email
+      this.telefone = telefone
+  }
+}
+
+export default UsuariosModel
