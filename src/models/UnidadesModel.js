f098910e98f7babfdb25566bdffa541958751294
Caroli-ne <karoline_gnn@hotmail.com>
2023-09-16 15:28:31 -0300

    feat: criação do arquivo UnidadesModel, que contém a model Unidades

---
+++ src/models/UnidadesModel.js
@@ -0,0 +1,17 @@
+/**
+ * Objeto modelo para usuários
+ */
+class UnidadesModel{
+    /**
+     * Construtor do objeto modelo para usuários
+     * @param {string} nome 
+     * @param {string} IdEndereco
+    
+     */
+    constructor(nome, IdEndereco){
+        this.nome = nome
+        this.IdEndereco = IdEndereco
+    }
+}
+
+export default UnidadesModel
