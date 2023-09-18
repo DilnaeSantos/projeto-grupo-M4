dea9565fc621045080defd473995842e54eb5730
pan-ti <pandeandromeda@gmail.com>
2023-09-18 14:39:37 -0300

    Correções de sintaxe e adição de comentários

--- src/controllers/ProdutoControllers.js
+++ src/controllers/ProdutoControllers.js
@@ -1,14 +1,21 @@
+
 import ProdutoModel from "../models/ProdutoModel.js";
 import ValidacaoServices from "../services/ValidacaoServices.js";
 import ProdutoDAO from "../DAO/ProdutoDAO.js";
 
 class ProdutoController {
+    /**
+     * Método para centralização de rotas no controller
+     * @param {Express} app 
+     */
     static rotas(app) {
+        // Rota para buscar todos os produtos
         app.get("/produtos", async (req, res) => {
             const produtos = await ProdutoDAO.buscarTodosOsProdutos();
             res.status(200).json(produtos);
         });
 
+        // Rota para buscar um produto pelo ID
         app.get("/produtos/:id", async (req, res) => {
             const id = req.params.id;
             const isValid = await ValidacaoServices.validarExistenciaProduto(id);
@@ -26,6 +33,7 @@ class ProdutoController {
             }
         });
 
+        // Rota para deletar um produto pelo ID
         app.delete("/produtos/:id", async (req, res) => {
             const id = req.params.id;
             const isValid = await ValidacaoServices.validarExistenciaProduto(id);
@@ -38,12 +46,13 @@ class ProdutoController {
             }
         });
 
+        // Rota para inserir um novo produto
         app.post("/produtos", async (req, res) => {
             const body = req.body;
-            const isValid = ValidacaoServices.validaCamposProduto(body.NOME, body.DESCRICAO, body.PRECO, body.QTDEESTOQUE, body.EMAILARTESAO);
+            const isValid = ValidacaoServices.validaCamposProduto(...body);
 
             if (isValid) {
-                const produtoModelado = new ProdutoModel(body.NOME, body.DESCRICAO, body.PRECO, body.QTDEESTOQUE, body.EMAILARTESAO);
+                const produtoModelado = new ProdutoModel(...body);
 
                 try {
                     await ProdutoDAO.inserirProduto(produtoModelado);
@@ -59,6 +68,7 @@ class ProdutoController {
             }
         });
 
+        // Rota para atualizar um produto pelo ID
         app.put("/produtos/:id", async (req, res) => {
             const id = req.params.id;
             const body = req.body;
@@ -76,8 +86,9 @@ class ProdutoController {
             } else {
                 res.status(404).json({ error: true, message: `Produto não encontrado para o id ${id}` });
             }
-        })
+        });
     }
 }
 
 export default ProdutoController;
+
