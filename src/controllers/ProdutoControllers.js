86c2487d16df620825f3c6762b26adacb6457f68
pan-ti <pandeandromeda@gmail.com>
2023-09-19 15:30:52 -0300

    Adicionado rota para buscar artesão, postagem e atualizações

--- src/controllers/ProdutoControllers.js
+++ src/controllers/ProdutoControllers.js
@@ -1,4 +1,3 @@
-
 import ProdutoModel from "../models/ProdutoModel.js";
 import ValidacaoServices from "../services/ValidacaoServices.js";
 import ProdutoDAO from "../DAO/ProdutoDAO.js";
@@ -16,7 +15,7 @@ class ProdutoController {
         });
 
         // Rota para buscar um produto pelo ID
-        app.get("/produtos/:id", async (req, res) => {
+        app.get("/produto/:id", async (req, res) => {
             const id = req.params.id;
             const isValid = await ValidacaoServices.validarExistenciaProduto(id);
 
@@ -33,6 +32,24 @@ class ProdutoController {
             }
         });
 
+        // Rota para buscar produtos pelo artesao
+
+        app.get("/produtos/:email", async (req, res) => {
+            const email = req.params.email;
+            const isValid = await ValidacaoServices.validarExistenciaEmailArtesaoEmProduto(email); 
+                if (isValid) {
+                    const resposta = await ProdutoDAO.buscarProdutoPorArtesao(email);
+                        if (resposta) { 
+                            res.status(200).json(resposta);
+                        } else {
+                            res.status(404).json({ error: true, message: `Produto não encontrado para o email ${email}` });
+                        }
+                } else {
+                    res.status(400).json({ error: true, message: `Email inválido: ${email}` });
+                }
+        })
+
+
         // Rota para deletar um produto pelo ID
         app.delete("/produtos/:id", async (req, res) => {
             const id = req.params.id;
@@ -48,7 +65,7 @@ class ProdutoController {
 
         // Rota para inserir um novo produto
         app.post("/produtos", async (req, res) => {
-            const body = req.body;
+            const body = Object.values(req.body)
             const isValid = ValidacaoServices.validaCamposProduto(...body);
 
             if (isValid) {
@@ -73,11 +90,11 @@ class ProdutoController {
             const id = req.params.id;
             const body = req.body;
             const exists = await ValidacaoServices.validarExistenciaProduto(id);
-            const isValid = ValidacaoServices.validaCamposProduto(body.NOME, body.DESCRICAO, body.PRECO, body.QTDEESTOQUE, body.EMAILARTESAO);
+            const isValid = ValidacaoServices.validaCamposProduto(body.NOME, body.DESCRICAO, body.PRECO, body.QTDESTOQUE, body.EMAIL_ARTESAO);
 
             if (exists) {
                 if (isValid) {
-                    const produtoModelado = new ProdutoModel(body.NOME, body.DESCRICAO, body.PRECO, body.QTDEESTOQUE, body.EMAILARTESAO);
+                    const produtoModelado = new ProdutoModel(body.NOME, body.DESCRICAO, body.PRECO, body.QTDEESTOQUE, body.EMAIL_ARTESAO);
                     ProdutoDAO.atualizarProdutoPorId(id, produtoModelado);
                     res.status(204).json();
                 } else {
@@ -91,4 +108,5 @@ class ProdutoController {
 }
 
 export default ProdutoController;
+
 
