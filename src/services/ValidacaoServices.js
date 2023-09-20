891bf9a7032cb61596e75eaf6938752414ccc5ea
YohanBZ <barbozayohan@gmail.com>
2023-09-20 16:09:53 -0300

    feat: :zap: Atualizações nas funcoes de validação e adicionado as de produto , endereço, artesão e unidade

--- src/services/ValidacaoServices.js
+++ src/services/ValidacaoServices.js
@@ -1,7 +1,11 @@
+import ArtesaosDAO from "../DAO/ArtesaosDAO.js";
+import EnderecosDAO from "../DAO/EnderecoDAO.js"
+import ProdutosDAO from "../DAO/ProdutoDAO.js";
+import UnidadesDAO from "../DAO/UnidadeDAO.js";
 import UsuariosDAO from "../DAO/UsuariosDAO.js";
 
 class ValidacaoServices{
-    /**
+    /*
      * Método que valida a existencia do usuário na base de dados
      * @param {string} id 
      * @returns {boolean}
@@ -12,13 +16,75 @@ class ValidacaoServices{
     }
 
     /**
+     * Método que valida a existencia do produto na base de dados
+     * @param {string} id 
+     * @returns {boolean}
+     */
+    static async validarExistenciaProduto(id) {
+        const produto = await ProdutosDAO.buscarProdutoPorId(id);
+        return produto ;
+    }
+
+    /**
+     * Método que valida a existencia do endereço na base de dados
+     * @param {string} id 
+     * @returns {boolean}
+     */
+    static async validarExistenciaEndereco(id) {
+        const endereco = await EnderecosDAO.buscarEnderecoPorId(id);
+        return endereco ;
+    }
+    
+    /**
+     * Método que valida a existencia do usuário na base de dados
+     * @param {string} id 
+     * @returns {boolean}
+     */
+    static async validarExistenciaUnidade(id) {
+        const unidade = await UnidadesDAO.buscarUnidadePorId(id);
+        return unidade ;
+    }
+
+    /**
+     * Método que valida a existencia do artesão na base de dados
+     * @param {string} email
+     * @returns {boolean}
+     */
+    static async validarExistenciaEmail(email) {
+        const artesao = await ArtesaosDAO.buscarArtesaoPorEmail(email);
+        return artesao ;
+    }
+
+    /**
+     * Método que valida a existencia do produto pelo email do artesao na base de dados
+     * @param {string} email
+     * @returns {boolean}
+     */
+    static async validarExistenciaEmailArtesaoEmProduto(email) {
+        const artesao = await ProdutosDAO.buscarProdutoPorArtesao(email);
+        return artesao ;
+    }
+
+    /**
      * Método de validação de nome
      * @param {string} nome 
      * @returns {boolean}
      */
+
     static validaNome(nome){
-        return typeof nome == "string" && nome.length > 2
+        if (typeof nome !== "string") return false;
+        
+        const trimmedName = nome.trim();
+    
+        // Verifica se o nome tem um tamanho adequado (por exemplo, entre 3 e 50 caracteres)
+        if (trimmedName.length < 3 || trimmedName.length > 50) return false;
+    
+        // Verifica se o nome contém apenas caracteres alfabéticos e espaços
+        const regex = /^[a-zA-ZáÁéÉíÍóÓúÚàÀèÈìÌòÒùÙçÇãõÃÕâêôÂÊÔ ]+$/;
+        return regex.test(trimmedName);
     }
+    
+
 
     /**
      * Método para validação de email
@@ -26,7 +92,8 @@ class ValidacaoServices{
      * @returns {boolean}
      */
     static validaEmail(email){
-        return typeof email == "string" && email.length > 2
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        return typeof email === "string" && email.length > 2 && emailRegex.test(email);
     }
 
     /**
@@ -35,8 +102,17 @@ class ValidacaoServices{
      * @returns {boolean}
      */
     static validaTelefone(telefone){
-        const telefoneInt = parseInt(telefone)
-        return typeof telefone == "string" && telefone.length > 9 && telefone == telefoneInt
+        // Verifica se é uma string
+        if (typeof telefone !== "string") return false;
+
+        // Remove caracteres de formatação comuns
+        const limpo = telefone.replace(/[-() ]/g, '');
+
+        // Verifica se depois de remover caracteres de formatação, a string só tem dígitos
+        if (!limpo.split('').every(char => char >= '0' && char <= '9')) return false;
+
+        // Verifica se tem um tamanho válido (por exemplo, entre 10 e 11 dígitos para telefones brasileiros)
+        return limpo.length >= 10 && limpo.length <= 11;
     }
 
     /**
@@ -48,6 +124,26 @@ class ValidacaoServices{
      */
     static validaCamposUsuario(nome, email, telefone){
         const isValid = this.validaNome(nome) && this.validaEmail(email) && this.validaTelefone(telefone)
+        return isValid
+    }
+
+    static validaCamposArtesao(nome, email, telefone, tipoDeArte, bio){
+        const isValid = this.validaNome(nome) && this.validaEmail(email) && this.validaTelefone(telefone) && tipoDeArte != undefined && bio != undefined
+        return isValid
+    }
+
+    static validaCamposProduto(nome, descricao, preco, qtdestoque, emailArtesao){
+        const isValid = this.validaNome(nome) && descricao != undefined && preco != undefined && qtdestoque != undefined && this.validaEmail(emailArtesao)
+        return isValid
+    }
+
+    static validaCamposEndereco(cep, rua, numero, cidade, bairro){
+        const isValid = cep != undefined && rua != undefined && numero != undefined && cidade != undefined && bairro != undefined
+        return isValid
+    }
+
+    static validaCamposUnidade(nome, idEndereco){
+        const isValid = this.validaNome(nome) && idEndereco != undefined 
         return isValid
     }
 }
