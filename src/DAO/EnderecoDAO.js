bb7fba81bddaa616aef8d867745abc197478dc39
L4urenmartins <lauren_martins_borba@hotmail.com>
2023-09-19 21:24:12 -0300

    featch: Corrigindo estrutura da tabela Endereco e corrigindo a sintaxe do código.

--- src/DAO/EnderecoDAO.js
+++ src/DAO/EnderecoDAO.js
@@ -1,68 +1,69 @@
-import EnderecoModel from "../models/EnderecoModel.js";
-import DAO from "./DAO.js";
+import EnderecoModel from "../models/EnderecoModel.js"
+import DAO from "./DAO.js"
 
-class EnderecoDAO extends DAO {
+class EnderecosDAO extends DAO {
     /**
-     * Método de inserção de dados da tabela Endereços
+     * Método de inserção de dados da tabela Endereco
      * @param {EnderecoModel} data 
      */
     static async inserirEndereco(data) {
-        const dataValues = Object.values(data);
+        const dataValues = Object.values(data)
         const query = `
-        INSERT INTO ENDERECOS (RUA, CIDADE, CEP) VALUES (?,?,?)
-        `;
-        const result = await this.inserir(query, dataValues);
-        return result;
+          INSERT INTO ENDERECO (CEP, RUA, NUMERO, CIDADE, BAIRRO) VALUES (?,?,?,?,?)
+          `
+        const result = await this.inserir(query, dataValues)
+        return result
     }
 
     /**
      * Método que retorna todos os registros da tabela Endereços
-     * @returns {Array<EnderecoModel>}
+     * @returns {Array<EnderecosModel>}
      */
     static async buscarTodosOsEnderecos() {
         const query = `
-        SELECT * FROM ENDERECOS
-        `;
+      SELECT * FROM ENDERECO
+      `;
         const result = await this.buscar(query);
         return result;
     }
 
     /**
-     * Método de busca de registros na tabela Endereços através de um identificador
-     * @param {string} id 
-     * @returns {EnderecoModel}
-     */
+      * Método de busca de registros específicos na tabela endereço através de um identificador
+      * @param {string} id 
+      * @returns {UsuariosModel}
+      */
     static async buscarEnderecoPorId(id) {
         const query = `
-        SELECT * FROM ENDERECOS WHERE ID_ENDERECO = ?
+        SELECT * FROM ENDERECO WHERE ID_ENDERECO = ?
         `;
         const result = await this.buscarPorId(query, [id]);
         return result;
     }
 
     /**
-     * Método de deleção de registros na tabela Endereços através de um identificador
-     * @param {string} id 
-     */
+      * Método de deleção de registros específicos na tabela Endereco através de um identificador
+      * @param {string} id 
+      */
     static async deletarEnderecoPorId(id) {
         const query = `
-        DELETE FROM ENDERECOS WHERE ID_ENDERECO = ?
-        `;
+        DELETE FROM ENDERECO WHERE ID_ENDERECO = ?
+          `;
         await this.deletarPorId(query, [id]);
     }
 
     /**
-     * Atualiza um registro da tabela Endereços através de um identificador
+     * Atualiza um registro específico da tabela Endereco através de um identificador
      * @param {string} id 
      * @param {any} data 
-     */
+    */
     static async atualizarEnderecoPorId(id, data) {
         const query = `
-        UPDATE ENDERECOS SET RUA = ?, CIDADE = ?, CEP = ? WHERE ID_ENDERECO = ?
-        `;
-        const values = [data.rua, data.cidade, data.cep, id];
+      UPDATE ENDERECO SET CEP = ?, RUA = ?, NUMERO = ?, CIDADE = ?, BAIRRO = ?WHERE ID_ENDERECO = ?
+          `;
+        const values = [data.cep, data.rua, data.numero, data.cidade, data.bairro, id];
         await this.atualizarPorId(query, values);
     }
+
 }
 
-export default EnderecoDAO;
+export default EnderecosDAO
