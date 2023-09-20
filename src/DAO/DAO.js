9aea6a83d7fa1b95a5861254ed92e16897391010
YohanBZ <barbozayohan@gmail.com>
2023-09-20 14:43:23 -0300

    feat: :zap: adicionado as funcoes de buscar, deletar e atualizar por email

--- src/DAO/DAO.js
+++ src/DAO/DAO.js
@@ -1,19 +1,19 @@
 import Database from "../database/Database.js";
 
-class DAO {
+class DAO{
     /**
      * Método de inserção de dados
      * @param {string} query 
      * @param {Array<any>} data 
      */
-    static inserir(query, data) {
-        return new Promise((resolve, reject) => {
-            Database.run(query, data, (error) => {
-                if (error) {
+    static inserir(query, data){
+        return new Promise((resolve, reject)=>{
+            Database.run(query, data, (error)=>{
+                if(error){
                     console.log(error)
                     reject(error)
                 }
-                resolve({ error: false })
+                resolve({error:false})
             })
         })
     }
@@ -23,10 +23,10 @@ class DAO {
      * @param {string} query
      * @returns {any}
      */
-    static buscar(query) {
-        return new Promise((resolve, reject) => {
-            Database.all(query, (error, rows) => {
-                if (error) {
+    static buscar(query){
+        return new Promise((resolve, reject)=>{
+            Database.all(query, (error, rows)=>{
+                if(error){
                     console.log(error)
                     reject(error)
                 } else {
@@ -44,7 +44,27 @@ class DAO {
      */
     static buscarPorId(query, id) {
         return new Promise((resolve, reject) => {
-            Database.get(query, id, (error, row) => {
+        Database.get(query, id, (error, row) => {
+            if (error) {
+                  console.error(error);
+                  reject(error);
+              } else {
+                  resolve(row);
+              }
+          });
+      });
+    }
+
+    /**
+     * Método de busca de dados específicos através de um identificador
+     * @param {string} query - A consulta SQL
+     * @param {string} email- Parâmetros da consulta
+     * @returns {any}
+     */
+    static buscarPorEmail(query, email) {
+        return new Promise((resolve, reject) => {
+        //   const query = "SELECT * FROM ARTESAO WHERE EMAIL_ARTESAO = ?";
+            Database.get(query, email, (error, row) => {
                 if (error) {
                     console.error(error);
                     reject(error);
@@ -55,6 +75,24 @@ class DAO {
         });
     }
 
+    /**
+     * Método de busca de dados específicos através de um identificador
+     * @param {string} query - A consulta SQL
+     * @param {string} email- Parâmetros da consulta
+     * @returns {any}
+     */
+    static buscarProdutoPorEmailArtesao(query, email) {
+      return new Promise((resolve, reject) => {
+            Database.all(query, email, (error, rows) => {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                } else {
+                    resolve(rows);
+                }
+            });
+        });
+      }
 
     /**
      * Método de deleção de dados específicos
@@ -63,17 +101,36 @@ class DAO {
      * @returns {Promise<void>}
      */
     static deletarPorId(query, id) {
-        return new Promise((resolve, reject) => {
-            Database.run(query, id, function (error) {
-                if (error) {
-                    console.error(error);
-                    reject(error);
-                } else {
-                    resolve();
-                }
-            });
-        });
+      return new Promise((resolve, reject) => {
+          Database.run(query, id, function (error) {
+              if (error) {
+                  console.error(error);
+                  reject(error);
+              } else {
+                  resolve();
+              }
+          });
+      });
     }
+
+    /**
+     * Método de deleção de dados específicos
+     * @param {string} query - A consulta SQL
+     * @param {email} email - Parâmetros da consulta
+     * @returns {Promise<void>}
+     */
+        static deletarPorEmail(query, email) {
+          return new Promise((resolve, reject) => {
+              Database.run(query, email, function (error) {
+                  if (error) {
+                      console.error(error);
+                      reject(error);
+                  } else {
+                      resolve();
+                  }
+              });
+          });
+        }
 
     /**
      * Atualiza um registro específico na base de dados
@@ -82,17 +139,36 @@ class DAO {
      * @returns {Promise<void>}
      */
     static atualizarPorId(query, id) {
-        return new Promise((resolve, reject) => {
-            Database.run(query, id, function (error) {
-                if (error) {
-                    console.error(error);
-                    reject(error);
-                } else {
-                    resolve();
-                }
-            });
-        });
+      return new Promise((resolve, reject) => {
+          Database.run(query, id, function (error) {
+              if (error) {
+                  console.error(error);
+                  reject(error);
+              } else {
+                  resolve();
+              }
+          });
+      });
     }
+
+    /**
+     * Atualiza um registro específico na base de dados
+     * @param {string} query - A consulta SQL
+     * @param {string} email - Parâmetros da consulta
+     * @returns {Promise<void>}
+     */
+        static atualizarPorEmail(query, email) {
+          return new Promise((resolve, reject) => {
+              Database.run(query, email, function (error) {
+                  if (error) {
+                      console.error(error);
+                      reject(error);
+                  } else {
+                      resolve();
+                  }
+              });
+          });
+        }
 
 }
 
